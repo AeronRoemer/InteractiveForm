cd7be0ca19afd689b139059f5cb0426f5debf105
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-01-09 16:19:25 -0700

    Shirt Selection Update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
+//initial display changes made with jQuery.
+//without these, site will dislay plain HTML design.
 $('#name').focus();
 $('#other-title').hide();
+$('#color').children('option').hide();
+
+//displays & hides 'Other' textarea based on user choice of Job Title.
 $('#title').change(function(){
     if ($(this).val() == 'other'){
      $('#other-title').show();
     } else if ($(this).val() !== 'other'){
     $('#other-title').hide();
     }
+}
+)
+
+//displays and hides available colors based on user choice of design. 
+
+$('#design').change(function(){
+   if ($(this).val() == 'js puns'){
+       $('#color').children('option').show();
+       $('#color option[value=steelblue]').hide();
+       $('#color option[value=dimgrey]').hide();
+       $('#color option[value=tomato]').hide();
+    } else {
+        $('#color').children('option').show();
+        $('#color option[value=cornflowerblue]').hide();
+        $('#color option[value=darkslategrey]').hide();
+        $('#color option[value=gold]').hide();
+       }
 }
 )
